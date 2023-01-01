b84e9d96f546a2d065f489c78a70966d5e5bd9a1
Pranav Mathur <pranavmathur2.0@gmail.com>
2023-01-01 23:33:16 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const arr = [];
+
+function insert(num) {
+  arr.push(num);
+  let str = arr.join(" ")
+  document.querySelector(".textview").value = str;
+}
+
+function equals() {
+  let lhs, rhs;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] == "string" && arr[i] != ".") {
+      lhs = arr.slice(0, i);
+      lhs = lhs.join("");
+      lhs = parseFloat(lhs);
+      rhs = arr.slice(i + 1, arr.length);
+      rhs = rhs.join("");
+      rhs = parseFloat(rhs);
+      if (arr[i] == "+")
+        document.querySelector(".textview").value = lhs + rhs;
+      else if (arr[i] == "-")
+        document.querySelector(".textview").value = lhs - rhs;
+      else if (arr[i] == "*")
+        document.querySelector(".textview").value = lhs * rhs;
+      else if (arr[i] == "/")
+        document.querySelector(".textview").value = lhs / rhs;
+      else if (arr[i] == "%")
+        document.querySelector(".textview").value = lhs % rhs;
+    }
+  }
+}
+
+function clean() {
+  arr.splice(0, arr.length);
+  document.querySelector(".textview").value = "";
+}
+
+function back() {
+  arr.pop();
+  let str = arr.join(" ")
+  document.querySelector(".textview").value = str;
+}
